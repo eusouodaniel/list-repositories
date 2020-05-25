7a9958f5ecba89a69a31b92f7851593923a64e9a
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-05-25 00:13:21 -0300

    NEW: List open issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { IssueList, Loading, Owner } from './styles';
 
 class Repository extends Component {
     static propTypes = {
@@ -54,10 +54,26 @@ class Repository extends Component {
             <Container>
                 <Owner>
                     <Link to="/">Página inicial</Link>
-                    <img src={repository.owner.avatar_url} alt={repository.owner.login}></img>
+                    <img src={repository.owner.avatar_url} alt={repository.owner.login} />
                     <h1>{repository.name}</h1>
                     <p>{repository.description}</p>
                 </Owner>
+                <IssueList>
+                    {issues.map(issue => (
+                        <li key={String(issue.id)}>
+                            <img src={issue.user.avatar_url} alt={issue.user.login} />
+                            <div>
+                                <strong>
+                                    <a href={issue.html_url} rel="noopener noreferrer">{issue.title}</a>
+                                    {issue.labels.map(label => (
+                                        <span key={String(label.id)}>{label.name}</span>
+                                    ))}
+                                </strong>
+                                <p>{issue.user.login}</p>
+                            </div>
+                        </li>
+                    ))}
+                </IssueList>
             </Container>
         );
     }
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -41,3 +41,62 @@ export const Owner = styled.div`
         max-width: 400px;
     }
 `;
+
+export const IssueList = styled.ul`
+    padding-top: 30px;
+    margin-top: 30px;
+    border-top: 1px solid #eee;
+    list-style: none;
+
+    li {
+        display: flex;
+        padding: 15px 10px;
+        border: 1px solid #fff;
+        border-radius: 4px;
+    }
+
+    & + li {
+        margin-top: 10px;
+    }
+
+    img {
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+        border: 2px solid #fff;
+
+        div {
+            flex: 1;
+            margin-left: 15px;
+
+            strong {
+                font-size: 16px;
+
+                a {
+                    text-decoration: none;
+                    color: #fff
+
+                    &:hover {
+                        color: #4169E1;
+                    }
+                }
+
+                span {
+                    background: #fff;
+                    color: #333;
+                    border-radius: 2px;
+                    font-size: 12px;
+                    font-weight: 600;
+                    height: 20px;
+                    padding: 3px 4px;
+                }
+            }
+
+            p {
+                margin-top: 5px;
+                font-size: 12px;
+                color: #999;
+            }
+        }
+    }
+`;
